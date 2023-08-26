32f7aed1c90b4fc1db5c63e7d5ce67d3fa429d94
Osama-tec <hashmiosama555@gmail.com>
2023-08-26 22:45:21 +0500

    sifkkk

--- model/userSchema.js
+++ model/userSchema.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    unique: true,
   },
   email: {
     type: String,
-    unique: true,
   },
   password: {
     type: String,
