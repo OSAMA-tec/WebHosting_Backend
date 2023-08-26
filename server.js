a3811d8f1d46465debe2d56e159172e0a2ce0614
Osama-tec <hashmiosama555@gmail.com>
2023-08-26 23:02:04 +0500

    serverjs updated

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ app.use(express.json({ extended: false }));
 
 app.use('/api/users', userRoute);
 
+// Error middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: process.env.NODE_ENV === 'development' ? err : {},
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
